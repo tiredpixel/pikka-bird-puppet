12f0c814e4fdd08c6bb247c0b3264690a0a593cf
tiredpixel <tp@tiredpixel.com>
2015-07-12 17:21:38 +0100

    Tweak using puppet-lint feedback.

--- manifests/collector/install.pp
+++ manifests/collector/install.pp
@@ -18,24 +18,24 @@ class pikka_bird::collector::install inherits pikka_bird::collector {
 
   if $ensure != 'absent' and $pikka_bird::collector::path_manage {
     file { 'pikka-bird-collector-etc':
-      ensure  => $ensure_d,
-      mode    => $pikka_bird::collector::path_mode_dir,
-      owner   => $pikka_bird::collector::user_name,
-      path    => $pikka_bird::collector::path_etc,
+      ensure => $ensure_d,
+      mode   => $pikka_bird::collector::path_mode_dir,
+      owner  => $pikka_bird::collector::user_name,
+      path   => $pikka_bird::collector::path_etc,
     }
 
     file { 'pikka-bird-collector-conf':
-      ensure  => $ensure_d,
-      mode    => $pikka_bird::collector::path_mode_dir,
-      owner   => $pikka_bird::collector::user_name,
-      path    => $pikka_bird::collector::path_conf,
+      ensure => $ensure_d,
+      mode   => $pikka_bird::collector::path_mode_dir,
+      owner  => $pikka_bird::collector::user_name,
+      path   => $pikka_bird::collector::path_conf,
     }
 
     file { 'pikka-bird-collector-log':
-      ensure  => $ensure_d,
-      mode    => $pikka_bird::collector::path_mode_dir,
-      owner   => $pikka_bird::collector::user_name,
-      path    => $pikka_bird::collector::path_log,
+      ensure => $ensure_d,
+      mode   => $pikka_bird::collector::path_mode_dir,
+      owner  => $pikka_bird::collector::user_name,
+      path   => $pikka_bird::collector::path_log,
     }
   }
 
--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -10,17 +10,17 @@ class pikka_bird::server::install inherits pikka_bird::server {
 
   if $pikka_bird::server::path_manage {
     file { 'pikka-bird-server-etc':
-      ensure  => $ensure_d,
-      mode    => $pikka_bird::server::path_mode_dir,
-      owner   => $pikka_bird::server::user_name,
-      path    => $pikka_bird::server::path_etc,
+      ensure => $ensure_d,
+      mode   => $pikka_bird::server::path_mode_dir,
+      owner  => $pikka_bird::server::user_name,
+      path   => $pikka_bird::server::path_etc,
     }
 
     file { 'pikka-bird-server-log':
-      ensure  => $ensure_d,
-      mode    => $pikka_bird::server::path_mode_dir,
-      owner   => $pikka_bird::server::user_name,
-      path    => $pikka_bird::server::path_log,
+      ensure => $ensure_d,
+      mode   => $pikka_bird::server::path_mode_dir,
+      owner  => $pikka_bird::server::user_name,
+      path   => $pikka_bird::server::path_log,
     }
   }
 
