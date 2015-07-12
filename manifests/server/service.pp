a0a81bcb7b753ecc3cf0a22201d59e0fe4a88280
tiredpixel <tp@tiredpixel.com>
2015-07-12 16:35:25 +0100

    Fix uninstall service definition when ensure=absent .

--- manifests/server/service.pp
+++ manifests/server/service.pp
@@ -1,6 +1,6 @@
 class pikka_bird::server::service inherits pikka_bird::server {
 
-  if $service_manage == true {
+  if $ensure != 'absent' && $service_manage == true {
     service { 'pikka-bird-server':
       ensure     => $service_ensure2,
       enable     => $service_enable,
